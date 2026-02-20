5cbc000d0a4f84161ac4bbf40cc25ba2481425cf
Nicolas Brichet <nicolas.brichet@quantstack.net>
2026-02-20 14:14:46 +0100

    Remove tests

--- ui-tests/tests/jupyter_ai_chat_components.spec.ts
+++ ui-tests/tests/jupyter_ai_chat_components.spec.ts
@@ -1,51 +1,5 @@
 import { expect, test } from '@jupyterlab/galata';
 
-test('should display  data file', async ({ page }) => {
-  const filename = 'test';
-  await page.menu.clickMenuItem('File>New>Text File');
-
-  // Set MIME type content in fill
-  await page.getByRole('main').getByRole('textbox').fill('');
-
-  await page.menu.clickMenuItem('File>Save Text');
-
-  await page.locator('.jp-Dialog').getByRole('textbox').fill(filename);
-
-  await page.getByRole('button', { name: 'Rename' }).click();
-  await page.waitForTimeout(200);
-
-  // Close file opened as editor
-  await page.activity.closePanel('test.my_type');
-
-  await page.filebrowser.open(filename);
-
-  const view = page.getByRole('main').locator('.mimerenderer-');
-
-  expect(await view.screenshot()).toMatchSnapshot('-file.png');
-});
-
-test('should display notebook  output', async ({ page }) => {
-  await page.menu.clickMenuItem('File>New>Notebook');
-
-  await page.getByRole('button', { name: 'Select' }).click();
-
-  await page.notebook.setCell(
-    0,
-    'code',
-    `from IPython.display import display
-# Example of MIME type content
-output = {
-    "application/vnd.jupyter.chat.components": ""
-}
-
-display(output, raw=True)`
-  );
-
-  await page.notebook.run();
-
-  const outputs = page
-    .getByRole('main')
-    .locator('.mimerenderer-.jp-OutputArea-output');
-
-  await expect(outputs).toHaveCount(1);
+test('should be tested', async ({ page }) => {
+  expect(1).toEqual(1);
 });
